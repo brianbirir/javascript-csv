77692a9ecf7501a3b3496ac3d6927d16027c4230
brianbirir <brianbirir@gmail.com>
2016-05-26 06:56:57 +0300

    Add function to read from  json to open links based on url

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,43 +1,60 @@
 //Custom Javascript
 (function ($) {
-	 $(document).ready(function(){
-	 	//------OPEN PANEL-----
-		/*
-		This Function opens an accordion panel based on the URL
-		*/
+	 $(document).ready(function() {
 
-		//get path name from URL
-		var pathName = window.location.pathname;
+		// Load JSON Data from local file using loop
+		$.getJSON("links.json", function(data) {
+			for (var i = 0; i < data.length; i++) {
+				
+				// Store object
+				var jsonObj = data[i];
+				
+				// Get link property from json object
+				var hrefLink = jsonObj.link;
+				console.log(hrefLink); // For debugging
 
-		// initiate variable to store active panel value
-		var active_panel;
+				// Get secondary category from json object
+				var secondaryCategory = jsonObj.secondarycategory;
 
-		// Using the jQuery library
-		$('.results').html(pathName);
+				// Get current page address
+				var pageAddress = window.location.href;
 
-		switch(pathName) {
-			case "/~brianbirir/accordion-tests/index.html":
-				active_panel = 0;
-				break;
-			case "/~brianbirir/accordion-tests/about.html":
-				active_panel = 1;
-				break;
-			case "/~brianbirir/accordion-tests/contact.html":
-				active_panel = 2;
-				break;
-		}
-		// open the panel that is active
-		$(".accordion-sidebar").accordion(
-		{
-			collapsible: true,
-			header: "h3",
-			active: active_panel,
-			animated: false,
-			icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+				// check if link property is the same as page address
+				if(hrefLink == pageAddress) {
+					switch(secondaryCategory) {
+						case "Agro Ecological Zone":
+						active_panel = 0;
+						break;
+					case "Water Management":
+						active_panel = 1;
+						break;
+					case "Sustainable & Organic Agriculture":
+						active_panel = 2;
+						break;
+					case "Conservation Agriculture":
+						active_panel = 3;
+						break;
+					case "Agroforestry":
+						active_panel = 4;
+						break;
+					case "Trees":
+						active_panel = 5;
+						break;
+					case "Processing & Value Addition":
+						active_panel = 6;
+						break;
+					}
+
+					// open the panel that is active
+					$(".accordion-sidebar").accordion({
+						collapsible: true,
+						header: "h3",
+						active: active_panel,
+						animated: false,
+						icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+					});
+				}
+			}
 		});
-
-	//------END OF FUNCTION----
 	 });
 })(jQuery);
-
-/*Parse CSV*/
