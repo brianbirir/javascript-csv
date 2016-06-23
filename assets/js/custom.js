15e935bb8cd83f0073f6d52182c3318d9f040f9f
brianbirir <brianbirir@gmail.com>
2016-06-23 11:55:43 +0300

    Refactor code for jQuery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,4 @@
 //Custom Javascript
-(function ($) {
-	
-	$(document).ready(function() {
-
 	// Global variables
 	var jsonData;
 
@@ -56,59 +52,60 @@
 
 	}
 
-	function readJSON(jsonData) {
+function readJSON(jsonData) {
 
-		for (var i = 0; i < jsonData.length; i++) {
-			
-			var jsonObj = jsonData[i];
+	for (var i = 0; i < jsonData.length; i++) {
+		
+		var jsonObj = jsonData[i];
 
-			//console.log(jsonObj);
+		//console.log(jsonObj);
 
-			// Get link property from json object
-			var hrefLink = jsonObj.link;
-			console.log(hrefLink); // For debugging
+		// Get link property from json object
+		var hrefLink = jsonObj.link;
+		console.log(hrefLink); // For debugging
 
-			// Get secondary category from json object
-			var secondaryCategory = jsonObj.secondarycategory;
-			console.log(secondaryCategory); // For debugging
+		// Get secondary category from json object
+		var secondaryCategory = jsonObj.secondarycategory;
+		console.log(secondaryCategory); // For debugging
 
-			// Get current page address
-			var pageAddress = window.location.href;
+		// Get current page address
+		var pageAddress = window.location.href;
+		// console.log(pageAddress); // For debugging
 
-			// console.log(pageAddress); // For debugging
-
-			// open accordions based on link and secondary category
-			selectAccordion(hrefLink,secondaryCategory);
-		}
+		// open accordions based on link and secondary category
+		selectAccordion(hrefLink,pageAddress,secondaryCategory);
 	}
+}
 
-	function selectAccordion(hrefLink,secondaryCategory) {
-		// check if link property is the same as page address
-		if(hrefLink == pageAddress) {
-			switch(secondaryCategory) {
-				case "Agro Ecological Zone":
-				active_panel = 0;
-				break;
-			case "Water Management":
-				active_panel = 1;
-				break;
-			case "Sustainable & Organic Agriculture":
-				active_panel = 2;
-				break;
-			case "Conservation Agriculture":
-				active_panel = 3;
-				break;
-			case "Agroforestry":
-				active_panel = 4;
-				break;
-			case "Trees":
-				active_panel = 5;
-				break;
-			case "Processing & Value Addition":
-				active_panel = 6;
-				break;
-			}
+function selectAccordion(hrefLink,pageAddress,secondaryCategory) {
+	// check if link property is the same as page address
+	if(hrefLink == pageAddress) {
+		switch(secondaryCategory) {
+			case "Agro Ecological Zone":
+			active_panel = 0;
+			break;
+		case "Water Management":
+			active_panel = 1;
+			break;
+		case "Sustainable & Organic Agriculture":
+			active_panel = 2;
+			break;
+		case "Conservation Agriculture":
+			active_panel = 3;
+			break;
+		case "Agroforestry":
+			active_panel = 4;
+			break;
+		case "Trees":
+			active_panel = 5;
+			break;
+		case "Processing & Value Addition":
+			active_panel = 6;
+			break;
+		}
 
+		// accordion function
+		$( document ).ready(function(){
 			// open the panel that is active
 			$(".accordion-sidebar").accordion({
 				collapsible: true,
@@ -117,7 +114,6 @@
 				animated: false,
 				icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
 			});
-		}
+		});
 	}
-	 });
-})(jQuery);
+}
